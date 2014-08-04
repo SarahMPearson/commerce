23392b7aa53d8f4f583f8f9c387e6e08c0be8239
Sarah M Pearson <sarahp@me.com>
2014-08-04 10:22:05 -0500

    committing because we have to ssh out of computer.

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -14,3 +14,20 @@ exports.create = function(req, res){
   });
 };
 
+exports.index = function(req, res){
+  Item.all(function(items){
+    res.render('items/index', {items:items});
+  });
+};
+
+exports.show = function(req, res){
+  Item.findById(req.params.id, function(item){
+    res.render('items/show', {item:item});
+  });
+};
+
+exports.destroy = function(req, res){
+  Item.deleteById(req.params.id, function(){
+    res.redirect('/items');
+  });
+};
